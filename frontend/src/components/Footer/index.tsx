45cca4de3583e94dd4a776ad1e1efe7cff9348ff
Gabriel Costa <gabrielc_felippe@hotmail.com>
2021-09-09 21:56:46 -0300

    Gráficos Estáticos

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,4 +1,5 @@
 function Footer() {
+
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
@@ -7,7 +8,6 @@ function Footer() {
                     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
         </footer>
-
     );
 }
 
