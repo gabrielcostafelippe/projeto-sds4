b9d32c04bd7497bf34e828a02ea15b1341f75cc0
Gabriel Costa <gabrielc_felippe@hotmail.com>
2021-09-11 23:13:11 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,27 @@
+import Dashboard from 'pages/Dashboard';
+import Home from 'pages/Home';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+
+
+function Routes() {
+    return (
+            <BrowserRouter>
+
+            <Switch>
+
+                <Route path="/" exact>
+                    <Home/>
+                </Route>
+
+                <Route path="/dashboard">
+                    <Dashboard/>
+                </Route>
+
+            </Switch>
+
+            </BrowserRouter>
+    );
+}
+
+export default Routes;
